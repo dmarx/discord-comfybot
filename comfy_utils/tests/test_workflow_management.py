384caa2479f69e9ed64d819d5b650a19ed812ab6
David <david.marx84@gmail.com>
2024-03-03 13:42:02 -0800

    test change detection

--- comfy_utils/tests/test_workflow_management.py
+++ comfy_utils/tests/test_workflow_management.py
@@ -1,5 +1,8 @@
+import copy
 import pytest
+
 from comfy_utils.workflow_manager import Workflow, WorkflowManager
+
 
 def test_fetch():
     wf = Workflow(name="_api_default")
@@ -13,3 +16,35 @@ def test_lazyload():
     assert wf.data != {} # lazy loads when this attribute invoked
     assert wf._data != {}
     assert len(wf.data.values())
+
+
+#################################################################
+# modifying the workflow through bracket notation on the object # 
+# or via the .data attribute should trigger change detection    #
+#################################################################
+
+def test_change_detection__class_setter():
+    wf = Workflow(name="_api_default")
+    wf.fetch()
+    d0 = copy.deepcopy(wf._data)
+    assert d0 == wf._baseline
+    assert not wf._uncommitted_changes
+
+    wf['3'] = "foo"
+    assert wf._uncommitted_changes
+
+    d1 = copy.deepcopy(wf._data)
+    assert d0 == wf._baseline
+    assert d0 != d1
+
+
+# this test fails
+def test_change_detection__property_setter():
+    wf = Workflow(name="_api_default")
+    wf.fetch()
+    d0 = copy.deepcopy(wf._data)
+    assert d0 == wf._baseline
+    assert not wf._uncommitted_changes
+
+    wf.data['3'] = "foo"
+    assert wf._uncommitted_changes
